9eaf2a1f2d5a1986711e211146b4689f9273181e
Artem Yartsev <yartsev@tiburon-research.ru>
2026-02-04 11:15:19 +0400

    hometask_1

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand/v2"
+)
 
+const iterations = 10
+
+func main() {
+	intCh := make(chan int)
+	squareCh := make(chan int)
+	go getRandomInts(intCh)
+	go getSquares(intCh, squareCh)
+	var res []int
+
+	for v := range squareCh {
+		res = append(res, v)
+	}
+	for _, v := range res {
+		fmt.Printf("%d ", v)
+	}
+}
+
+func getRandomInts(out chan<- int) {
+	defer close(out)
+	var res []int
+	for range iterations {
+		res = append(res, rand.IntN(100))
+	}
+	for _, n := range res {
+		out <- n
+	}
+}
+
+func getSquares(in <-chan int, out chan<- int) {
+	defer close(out)
+	for n := range in {
+		out <- n * n
+	}
 }
